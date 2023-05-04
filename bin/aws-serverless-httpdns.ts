49fd6391e4c23dc3c0c0a244dd2e7691dd1cf4ff
xjiaqing <ballieasor@outlook.com>
2023-05-04 02:17:01 +0000

    add Amazon GA endpoint

--- bin/aws-serverless-httpdns.ts
+++ bin/aws-serverless-httpdns.ts
@@ -1,33 +1,8 @@
 #!/usr/bin/env node
 import * as cdk from 'aws-cdk-lib';
 import 'source-map-support/register';
-import { AwsServerlessHttpdnsRegionalStack } from '../lib/aws-serverless-httpdns-regional-stack';
+import { AwsServerlessHttpdnsStack } from '../lib/aws-serverless-httpdns-stack';
 
 
-const regions = ['us-east-1', 'ap-southeast-1', 'ap-northeast-1']
-for (let i = 0; i < regions.length; i++) {
-    const region = regions[i];
-    console.log('=====================' + region)
-
-    const app = new cdk.App();
-    const stack = new cdk.Stack(app, 'httpdns-serverless-stack-' + region, {
-        env: { region: region },
-        // crossRegionReferences: true
-    })
-
-    new AwsServerlessHttpdnsRegionalStack(stack, 'ServerlessHttpdnsStack-' + region)
-}
-
-
-// const app1 = new cdk.App()
-// const stack1 = new cdk.Stack(app1, 'httpdns-serverless-stack-us-east-1', { env: { region: 'us-east-1' }, crossRegionReferences: true })
-// new AwsServerlessHttpdnsRegionalStack(stack1, 'us-east-1-httpdns')
-
-// const app2 = new cdk.App()
-// const stack2 = new cdk.Stack(app2, 'httpdns-serverless-stack-ap-southeast-1', { env: { region: 'ap-southeast-1' }, crossRegionReferences: true })
-// new AwsServerlessHttpdnsRegionalStack(stack2, 'ap-southeast-1-httpdns')
-
-
-// const app3 = new cdk.App()
-// const stack3 = new cdk.Stack(app3, 'httpdns-serverless-stack-us-east-1', { env: { region: 'ap-northeast-1' }, crossRegionReferences: true })
-// new AwsServerlessHttpdnsRegionalStack(stack3, 'ap-northeast-1-httpdns')
+const app = new cdk.App();
+new AwsServerlessHttpdnsStack(app, 'AwsServerlessHttpdns')
--- lib/aws-serverless-httpdns-stack.ts
+++ lib/aws-serverless-httpdns-stack.ts
@@ -1,35 +1,21 @@
-// import * as cdk from 'aws-cdk-lib';
-// import { ApplicationLoadBalancer } from 'aws-cdk-lib/aws-elasticloadbalancingv2';
-// import { Accelerator } from 'aws-cdk-lib/aws-globalaccelerator';
-// import { ApplicationLoadBalancerEndpoint } from 'aws-cdk-lib/aws-globalaccelerator-endpoints';
-// import { Construct } from 'constructs';
-// import { AwsServerlessHttpdnsRegionalStack } from './aws-serverless-httpdns-regional-stack';
+import * as cdk from 'aws-cdk-lib';
+import { Accelerator } from 'aws-cdk-lib/aws-globalaccelerator';
+import { ApplicationLoadBalancerEndpoint } from 'aws-cdk-lib/aws-globalaccelerator-endpoints';
+import { Construct } from 'constructs';
+import { AwsServerlessHttpdnsRegionalStack } from './aws-serverless-httpdns-regional-stack';
 
-// interface endpointAlb {
-//     region: string,
-//     alb: ApplicationLoadBalancer
-// }
+export class AwsServerlessHttpdnsStack extends cdk.Stack {
 
-// export class AwsServerlessHttpdnsStack extends cdk.Stack {
+    constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+        super(scope, id, props);
 
-//     constructor(scope: Construct, id: string, props?: cdk.StackProps) {
-//         super(scope, id, props);
+        const regionalStack = new AwsServerlessHttpdnsRegionalStack(this, 'AwsServerlessHttpdnsStack')
 
-//         const regions = ['us-east-1', 'ap-southeast-1', 'ap-northeast-1']
-//         const endpointAlbs: endpointAlb[] = []
-//         regions.every((val) => {
-//             const httpdnsStack = new AwsServerlessHttpdnsRegionalStack(
-//                 this, 'AwsServerlessHttpdnsStack', { env: { region: val } }
-//             );
-//             endpointAlbs.push({ region: val, alb: httpdnsStack.alb })
-//         })
+        const accelerator = new Accelerator(this, 'Accelerator')
+        const listener = accelerator.addListener('Listener', { portRanges: [{ fromPort: 80 }] });
+        listener.addEndpointGroup('endpoint-1', { endpoints: [new ApplicationLoadBalancerEndpoint(regionalStack.alb)] })
 
-//         const accelerator = new Accelerator(this, 'Accelerator')
-//         const listener = accelerator.addListener('Listener', { portRanges: [{ fromPort: 80 }] });
-//         endpointAlbs.every((val) => {
-//             listener.addEndpointGroup(val.region, { endpoints: [new ApplicationLoadBalancerEndpoint(val.alb)] })
-//         })
-//         new cdk.CfnOutput(this, 'GA DNS name', { value: accelerator.dnsName })
-//         new cdk.CfnOutput(this, 'GA ARN', { value: accelerator.acceleratorArn })
-//     }
-// }
+        new cdk.CfnOutput(this, 'GA DNS name', { value: accelerator.dnsName })
+        new cdk.CfnOutput(this, 'GA ARN', { value: accelerator.acceleratorArn })
+    }
+}
